e4db3fa063637d2f3c05256e56d04f0790a89d61
MiBo3 <m.bojkovsky3@gmail.com>
2018-12-26 19:06:20 +0100

    mend

--- Main.py
+++ Main.py
@@ -72,22 +72,22 @@ if __name__ == "__main__":
     valid_labels, train_labels = ft.extract_multilingual_labels()
     valid_lang_labels, train_lang_labels = ft.extract_language_labels()
     # print(len(valid_labels), len(train_lang_labels), valid_sent.shape, train_sent.shape, valid_labels.shape, train_labels.shape)
-
+    #
     # get token lengths
     valid_token_lengths = [len(sent) for sent in valid_sent]
     train_token_lengths = [len(sent) for sent in train_sent]
     max_len = max(valid_token_lengths + train_token_lengths)
-
+    #
     # create indexed sentences
     valid_sent = w_emb.create_indexed_sentences(valid_sent, max_len)
     train_sent = w_emb.create_indexed_sentences(train_sent, max_len)
 
     # inicializacia modelu
-    batch_size = 50
+    batch_size = 64
     num_epochs = 100
     num_layers = 2
     num_units = 128
-    learning_rate = 0.003
+    learning_rate = 0.01
     word_dropout = 0.4
     lstm_dropout = 0.4
 
@@ -100,11 +100,10 @@ if __name__ == "__main__":
         num_layers=num_layers,
         num_units=num_units,
         learning_rate=learning_rate,
-        word_dropout=word_dropout,
         lstm_dropout=lstm_dropout)
 
-    m.train(train_sent[::16], train_labels[::16], train_token_lengths[::16], train_lang_labels[::16],
-            valid_sent[::16], valid_labels[::16], valid_token_lengths[::16], valid_lang_labels[::16])
-    # m.train(train_sent[:1000], train_labels[:1000], train_token_lengths[:1000], train_lang_labels[:1000],
-    #         valid_sent[:1000], valid_labels[:1000], valid_token_lengths[:1000], valid_lang_labels[:1000])
+    # m.train(train_sent[::16], train_labels[::16], train_token_lengths[::16], train_lang_labels[::16],
+    #         valid_sent[::16], valid_labels[::16], valid_token_lengths[::16], valid_lang_labels[::16])
+    m.train(train_sent[:1000], train_labels[:1000], train_token_lengths[:1000], train_lang_labels[:1000],
+            valid_sent[:1000], valid_labels[:1000], valid_token_lengths[:1000], valid_lang_labels[:1000])
     print('Done!')

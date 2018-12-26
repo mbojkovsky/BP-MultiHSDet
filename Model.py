bedf9e132799fdc8f89e0065d7c83566820c8f58
MiBo3 <m.bojkovsky3@gmail.com>
2018-12-26 19:06:01 +0100

    Fixed attention, temporarily removed discriminator and language extractor

--- Model.py
+++ Model.py
@@ -16,9 +16,9 @@ class Model:
                num_units=256,
                attention_size=512,
                learning_rate=0.001,
-               word_dropout=0.4,
                lstm_dropout=0.4,
-               adv_lambda=0.5
+               adv_lambda=0.5,
+               multi_lang=False
                ):
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -53,23 +53,14 @@ class Model:
     self.embedding = tf.nn.embedding_lookup(self.elmo_dic, self.x)
 
     # MULTILINGUAL FEATURE EXTRACTOR
-    self.cell = lambda: tf.contrib.cudnn_rnn.CudnnCompatibleLSTMCell(256)
-    self.f_cells_fw = [self.cell() for _ in range(1)]
-    self.f_cells_bw = [self.cell() for _ in range(1)]
+    self.cell = lambda: tf.contrib.cudnn_rnn.CudnnCompatibleLSTMCell(128)
 
-    with tf.variable_scope('feature_extractor'):
-      self.f_outputs, self.f_output_state_fw, self.f_output_state_bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
-        self.f_cells_fw,
-        self.f_cells_bw,
-        self.embedding,
-        dtype=tf.float32,
-        sequence_length=self.tokens_length
-      )
+    # todo: sem dat CNN
 
     # LANGUAGE DISCRIMINATOR
-    # todo: vyskusat treba aj CNN
     # todo: nejaky rozumnejsi pooling mozno najst
     # dopredu mi ide 1 * self.feature_out; dozadu mi ide -adv_lambda * self.feature_out
+    """
     self.gradient_reversal = tf.stop_gradient((1 + self.adv_lambda) * self.f_outputs) - self.adv_lambda * self.f_outputs
     # vytvorim 1024 rozmernu reprezentaciu pre kazdy jeden tweet
     self.d_pooled = tf.math.reduce_mean(self.gradient_reversal, axis=1)
@@ -80,6 +71,7 @@ class Model:
     # loss v diskriminatore
     self.disc_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.language_disc_out,
                                                                                    labels=self.lang_labels))
+    """
 
     # CELL AND LAYER DEFINITIONS
     # multilayer lstm
@@ -89,7 +81,7 @@ class Model:
     self.outputs, self.output_state_fw, self.output_state_bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
                                                                           self.cells_fw,
                                                                           self.cells_bw,
-                                                                          self.f_outputs,
+                                                                          self.embedding,
                                                                           dtype=tf.float32,
                                                                           sequence_length=self.tokens_length
                                                                           )
@@ -97,29 +89,24 @@ class Model:
 
     # ATTENTION
     # implementacia podla https://www.cs.cmu.edu/~hovy/papers/16HLT-hierarchical-attention-networks.pdf
-    self.word_context = tf.Variable(tf.random_normal([self.attention_size], stddev=0.1), trainable=True)
-    # jeden fully connected layer
+    self.word_context = tf.get_variable(name='Att', shape=[self.attention_size], dtype=tf.float32)
     self.att_activations = tf.layers.dense(self.outputs, self.attention_size, activation=tf.nn.tanh)
-    # vynasobenie s word_contextom
-    self.word_act = tf.tensordot(self.att_activations, self.word_context, axes=1)
-    # vypocitame attention koeficienty
-    self.coefs = tf.nn.softmax(self.word_act)
-    # aplikujeme koeficienty na output
-    self.out_with_att = self.outputs * tf.expand_dims(self.coefs, -1)
-    # poolneme
-    self.pooled = tf.reduce_sum(self.out_with_att, 1)
+    self.word_attn_vector = tf.reduce_sum(tf.multiply(self.att_activations, self.word_context), axis=2, keepdims=True)
+    self.att_weights = tf.nn.softmax(self.word_attn_vector, axis=1)
+    self.weighted_input = tf.multiply(self.outputs, self.att_weights)
+    self.pooled = tf.reduce_sum(self.weighted_input, axis=1)
+
+    self.pooled = tf.reduce_mean(self.outputs, axis=1)
 
     # TOP-LEVEL SOFTMAX LAYER
-    self.flat = tf.layers.flatten(self.pooled)
-
-    self.logits = tf.layers.dense(self.flat, units=2)
+    self.logits = tf.layers.dense(self.pooled, units=2)
     self.ce_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits,
                                                                                  labels=self.y))
 
     self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
     self.c_train_op = self.optimizer.minimize(self.ce_loss)
-    self.d_train_op = self.optimizer.minimize(self.disc_loss)
-    self.train_ops = tf.group(self.c_train_op, self.d_train_op)
+    if multi_lang:
+      self.d_train_op = self.optimizer.minimize(self.disc_loss)
 
     self.pred = tf.nn.softmax(self.logits)
 
@@ -150,7 +137,7 @@ class Model:
           start = iteration * self.batch_size
           end = min(start + self.batch_size, sentences.shape[0])
 
-          c_l, d_l, _ = sess.run([self.ce_loss, self.disc_loss, self.train_ops], feed_dict={self.x: sen[start:end],
+          c_l, d_l, _ = sess.run([self.ce_loss, self.ce_loss, self.c_train_op], feed_dict={self.x: sen[start:end],
                                                                                             self.y: lab[start:end],
                                                                                             self.tokens_length: t_lens[start:end],
                                                                                             self.lang_labels: l_lab[start:end]})
@@ -161,7 +148,7 @@ class Model:
         self.saver.save(sess, './chk/model_' + language)
         print('Epoch:', epoch, 'Classifier loss:', c_loss, 'Discriminator loss:', d_loss)
         print('TRAIN ACC:', self.test(sentences, labels, sentence_lengths, lang_labels))
-        print('VALID ACC', self.test(v_sentences, v_labels, v_sentence_lengths, v_lang_labels), '\n')
+        # print('VALID ACC', self.test(v_sentences, v_labels, v_sentence_lengths, v_lang_labels), '\n')
 
 
   def test(self, t_sentences, t_labels, t_sentence_lengths, t_lang_labels, language='en'):

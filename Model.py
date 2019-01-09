6f26067949caacb3c2f29a0b498dd2974653ae7d
MiBo3 <m.bojkovsky3@gmail.com>
2019-01-09 22:12:28 +0100

    Changed lstm level functionality (still broken)

--- Model.py
+++ Model.py
@@ -20,7 +20,6 @@ class Model:
                learning_rate=0.001,
                lstm_dropout=0.4,
                adv_lambda=0.2,
-               training=True
                ):
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -35,17 +34,16 @@ class Model:
     self.lstm_dropout = lstm_dropout
     self.adv_lambda = adv_lambda
     self.attention_size = attention_size
-    self.training = training
 
     self.word_embedder = word_embedder
     self.char_embedder = char_embedder
+    self.training = False
 
     # INPUTS
     # Sentence level
     self.sentences = tf.placeholder(tf.int32,
                             [None, w_max_len],
                             name='Sentences')
-
 
 
     self.w_tokens_length = tf.placeholder(tf.int32,
@@ -73,35 +71,53 @@ class Model:
                                             name='Embeddings')
 
     # CHAR EMBEDDINGS
-    self.c_dic_init = tf.constant_initializer(self.char_embedder.weights)
 
-    self.char_dic = tf.get_variable(shape=self.char_embedder.weights.shape,
-                                    initializer=self.c_dic_init,
-                                    trainable=True,
-                                    name='Character_weight_dict')
+    self.c_dic = tf.get_variable('char_embeddings',
+                                        shape=[self.char_embedder.weights.shape[0], self.char_embedder.weights.shape[1]],
+                                        trainable=True)
 
-    self.c_embedding = tf.nn.embedding_lookup(self.char_dic,
-                                              self.words,
-                                              name='Char_embeddings')
+    self.c_embedding = tf.nn.embedding_lookup(self.c_dic,
+                                              self.words)
+
+    # self.c_embedding = tf.one_hot(self.words,
+    #                               depth=self.char_embedder.weights.shape[0])
+
+    print(self.c_embedding.shape, 'C EMB') # BATCH x W_MAX_LEN x C_MAX_LEN x EMB_SIZE
 
     # CELL AND LAYER DEFINITIONS
     # CHAR LAYER
-    self.c_cell = lambda: tf.contrib.rnn.LSTMBlockCell(32)
+    self.c_cell = lambda: tf.contrib.rnn.LSTMBlockCell(64)
     self.c_cells_fw = [self.c_cell() for _ in range(1)]
     self.c_cells_bw = [self.c_cell() for _ in range(1)]
 
-    self.c_flat = tf.reshape(self.c_embedding, shape=[-1, w_max_len, c_max_len * self.char_embedder.weights.shape[1]])
+    # self.c_flat = tf.reshape(self.c_embedding, shape=[-1, w_max_len, c_max_len * self.char_embedder.weights.shape[1]])
 
-    self.c_outputs, _ , _ = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
+    self.c_flat = tf.reshape(self.c_embedding, shape=[-1, c_max_len, self.char_embedder.weights.shape[1]])
+    print(self.c_flat.shape, 'C EMB AFTER RESHAPE') # BATCH * W_MAX_LEN x C_MAX_LEN x EMB_SIZE
+
+    self.c_outputs, self.fw, self.bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
         self.c_cells_fw,
         self.c_cells_bw,
         self.c_flat,
         dtype=tf.float32,
-        sequence_length=self.w_tokens_length,
         scope='char_level_lstm')
 
+    # self.c_outputs_mean = tf.concat([self.fw[0][0], self.bw[0][0]], axis=-1)
+    self.c_outputs_mean = tf.math.reduce_mean(self.c_outputs, axis=1)
+    print(self.c_outputs_mean.shape) # BATCH * W_MAX_LEN x 128
+
+    self.c_outputs_reshaped = tf.reshape(self.c_outputs_mean, shape=[-1, w_max_len, self.c_outputs_mean.shape[1]])
+    print(self.c_outputs_reshaped.shape) # BATCH x W_MAX_LEN x 128
+
     # concat char level features with extracted features from char level lstm
-    self.embedding = tf.concat([self.w_embedding, self.c_outputs], -1)
+
+    """
+    self.w_emb_dropped = tf.layers.dropout(self.w_embedding,
+                                           rate=0.5,
+                                           training=self.training)
+
+    self.embedding = tf.concat([self.w_emb_dropped, self.c_outputs_reshaped],
+                               -1)
 
     # WORD LAYER
     self.cell = lambda: tf.contrib.rnn.LSTMBlockCell(num_units)
@@ -119,11 +135,11 @@ class Model:
     # ATTENTION
     # implementacia podla https://www.cs.cmu.edu/~hovy/papers/16HLT-hierarchical-attention-networks.pdf
     self.word_context = tf.get_variable(name='Att',
-                                        shape=[20],
+                                        shape=[w_max_len],
                                         dtype=tf.float32)
 
     self.att_activations = tf.contrib.layers.fully_connected(self.outputs,
-                                                             20,
+                                                             w_max_len,
                                                              weights_initializer=identity_init)
 
     self.word_attn_vector = tf.reduce_sum(tf.multiply(self.att_activations,
@@ -139,9 +155,11 @@ class Model:
 
     self.pooled = tf.reduce_sum(self.weighted_input,
                                 axis=1)
+"""
+    self.todo = tf.layers.dense(tf.layers.flatten(self.c_outputs_reshaped), units=256)
 
     # TOP-LEVEL SOFTMAX LAYER
-    self.logits = tf.layers.dense(self.pooled,
+    self.logits = tf.layers.dense(self.todo,
                                   units=2)
 
     self.ce_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.logits,
@@ -164,6 +182,7 @@ class Model:
     print('Started training!')
 
     iterations = np.arange(ceil(sentences.shape[0] / self.batch_size))
+    emb = []
 
     with tf.Session() as sess:
       # self.saver.restore(sess, './chk/model_' + language)
@@ -183,7 +202,8 @@ class Model:
           c_l, _ = sess.run([self.ce_loss, self.c_train_op], feed_dict={self.sentences: sen[start:end],
                                                                         self.labels: lab[start:end],
                                                                         self.words: char[start:end],
-                                                                        self.w_tokens_length: t_lens[start:end]})
+                                                                        self.w_tokens_length: t_lens[start:end]
+                                                                        })
 
           c_loss += c_l
 
@@ -207,7 +227,8 @@ class Model:
         corr_pred = sess.run([self.correct_prediction], feed_dict={self.sentences: t_sentences[start:end],
                                                                    self.w_tokens_length: t_sentence_lengths[start:end],
                                                                    self.words: t_words[start:end],
-                                                                   self.labels: t_labels[start:end]})
+                                                                   self.labels: t_labels[start:end]
+                                                                   })
         correct += corr_pred[0].sum()
     return correct / len(t_sentences)
  
